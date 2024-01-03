7d10fdd10333887d33c9eb6fb7904b6dcc3c8c5c
Shhubm <93233348+Shhubm@users.noreply.github.com>
2024-01-03 18:15:49 +0000

    String methods

---
+++ 01_basics/stacks_and_heaps.js
@@ -0,0 +1,7 @@
+const name = "shhubm "
+const repoCount = "10"
+
+// console.log("name"+"repoCount") there's a better way to do stings
+
+console.log(`my name is ${name} and my repo count is${repoCount}`);
+// iska kya scene hai k you have to use back ticks bole toh (  ``  ) ye or (${}) ye variable dalne k lea
